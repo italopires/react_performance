d1aa7c7506f771de28c16254638b43a122a950ab
Italo Pires <italonapalm@gmail.com>
2025-07-07 10:52:05 -0300

    Add throttle function in order to reduce the amount of times by time a function will called

--- throttling/src/hooks/useMousePosition.js
+++ throttling/src/hooks/useMousePosition.js
@@ -1,19 +1,21 @@
 import { useEffect, useState } from 'react';
+import { throttle } from '../helpers/throttle';
 
 export const useMousePosition = (options) => {
+  const throttleTime = options?.throttleTime || 1000;
   const [position, setPosition] = useState({
     x: 0,
     y: 0,
   });
 
   useEffect(() => {
-    const onMouseMove = (e) => {
+    const onMouseMove = throttle((e) => {
       const { clientX: x, clientY: y } = e;
       setPosition({
         x,
         y,
       });
-    };
+    }, throttleTime);
 
     window.addEventListener('mousemove', onMouseMove);
     return () => window.removeEventListener('mousemove', onMouseMove);
